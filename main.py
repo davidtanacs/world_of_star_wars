6b84fdcdffa1a0aeb7691ea1a72c116ea8af52ed
davidtanacs <tanacsdavid90@gmail.com>
2017-06-27 14:04:41 +0200

    now next and previous button works, but needs exception handling.

--- main.py
+++ main.py
@@ -4,18 +4,24 @@ import requests
 app = Flask(__name__)
 
 
+page_num = 1
+
+
 def main():
     show_table()
 
 
 @app.route('/')
 def show_table():
-    response = requests.get('http://swapi.co/api/planets/').json()
+    global page_num
+    if request.args.get('next'):
+        page_num += 1
+    elif request.args.get('previous'):
+        page_num -= 1
+    source = 'http://swapi.co/api/planets/?page={}'.format(page_num)
+    response = requests.get(source).json()
     table = response['results']
     return render_template('table.html', table=table)
-
-
-
 
 
 
