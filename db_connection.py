f5821699eb57d0a99b9e270b43d61b39aa435a85
davidtanacs <tanacsdavid90@gmail.com>
2017-07-01 12:18:54 +0200

    Db connection string typos fixed.

--- db_connection.py
+++ db_connection.py
@@ -8,11 +8,7 @@ def connect_to_db():
     try:
         urllib.parse.uses_netloc.append('postgres')
         url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        connection = psycopg2.connect(
-                                    database=dbname,
-                                    user=user,
-                                    password=password,
-                                    host='localhost')
+        connection = psycopg2.connect("database=dbname, user=user, password=password, host='localhost'")
         connection.autocommit = True
     except psycopg2.Error:
         print("Hey buddy, you made mistake(s) in config.py.")
