d883ea5fde69e17ccb3610622daf6e09e8c96ff8
davidtanacs <tanacsdavid90@gmail.com>
2017-07-01 21:22:28 +0200

    made changes in db connect.

--- db_connection.py
+++ db_connection.py
@@ -7,14 +7,9 @@ def connect_to_db():
     try:
         urllib.parse.uses_netloc.append('postgres')
         url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
-        connection = psycopg2.connect(
-            database=url.path[1:],
-            user=url.username,
-            password=url.password,
-            host=url.hostname,
-            port=url.port
-                )
-        connection.autocommit = True
+        conn = psycopg2.connect(database=url.path[1:], user=url.username, password=url.password, host=url.hostname,
+                                port=url.port)
+        conn.autocommit = True
     except psycopg2.Error:
         print("Hey buddy, you made mistake(s) in config.py.")
-    return connection
+    return conn
